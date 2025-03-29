cfdaa5a07c52047fa2373339a4e517e53b7890b9
Pavle Panic <pavlepanic2@gmail.com>
2025-03-29 23:27:29 +0100

    update

--- Word guessing game.py
+++ Word guessing game.py
@@ -2,3 +2,4 @@ import random
 name = input("Enter your name here: ")
 print("Welcome to the Word Guessing game! Good luck, {name}!")
 words = [shoe, bed, camp, wife, life,]
+word = random.randrange(words)
